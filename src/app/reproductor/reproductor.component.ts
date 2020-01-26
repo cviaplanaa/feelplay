1a37ca62c4e75ed7f299c1827c36fb3f8ccf8f50
MOI-GOM <moises.gomez.dev@gmail.com>
2020-01-26 22:34:00 +0100

    v2.0.3

--- src/app/reproductor/reproductor.component.ts
+++ src/app/reproductor/reproductor.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reproductor.component.css']
 })
 export class ReproductorComponent implements OnInit {
-
+  reproducir() {
+      const audio = new Audio('assets/GetLucky.mp3');
+      audio.play();
+  }
   constructor() { }
 
   ngOnInit() {
